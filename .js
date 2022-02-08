f00f3e3714176128193b584e59ad91082f9046c0
mehmet ali <90866582+05mehmet05@users.noreply.github.com>
2022-02-08 22:33:37 +0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,40 @@
+function yazı() {
+    document.body.style.backgroundColor = "red"
+}
+
+function yazı1() {
+    document.body.style.backgroundColor = "yellow"
+
+}
+
+function yazı2() {
+    document.body.style.backgroundColor = "blue"
+}
+
+function yazı3() {
+    document.body.style.backgroundColor = "orange"
+}
+
+function yazı4() {
+    document.body.style.backgroundColor = "pink	"
+}
+
+function yazı5() {
+    document.body.style.backgroundColor = "purple"
+}
+
+function yazı6() {
+    document.body.style.backgroundColor = "brown"
+}
+
+function yazı7() {
+    document.body.style.backgroundColor = "white"
+}
+
+function yazı8() {
+    document.body.style.backgroundColor = "grey"
+}
+
+function yazı9() {
+    document.body.style.backgroundColor = "silver"
+}
